99d52c8a15a79a88b47fef4aba4326076258ce04
Dan <daniel_arc21@yahoo.com>
2022-01-31 17:08:16 +1300

    Declared Provider and CLI Details

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
 # Declare AWS Provider and CLI Credentials
+provider "aws" {
+  region     = "ap-southeast-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# DECLARED ACCESS KEY AND SECRET KEY AS VARIABLE. SECURED LIKE A USERNAME AND PASSWORD
+variable "access_key" {
+  description = "Access Key For AWS Cli"
+}
+
+variable "secret_key" {
+  description = "Secret Key for AWS Cli"
+  
+}
+
