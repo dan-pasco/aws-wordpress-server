cbcc7b80f1a4650a55c8cf5a69d23f4d68187834
Dan Pasco <daniel_arc21@yahoo.com>
2022-02-01 08:40:44 +1300

    VPC created

--- main.tf
+++ main.tf
@@ -1,17 +1,13 @@
 # Declare AWS Provider and CLI Credentials
 provider "aws" {
   region     = "ap-southeast-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
+  }
 
-# DECLARED ACCESS KEY AND SECRET KEY AS VARIABLE. SECURED LIKE A USERNAME AND PASSWORD
-variable "access_key" {
-  description = "Access Key For AWS Cli"
-}
 
-variable "secret_key" {
-  description = "Secret Key for AWS Cli"
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = "10.0.0.0/16"
   
+  tags = {
+    Name = "main"
+  }
 }
-
