586f89fb94f8101758122c015d66dec9d15a6c03
Dan Pasco <daniel_arc21@yahoo.com>
2022-02-07 22:47:01 +1300

    Almost ok

--- main.tf
+++ main.tf
@@ -3,11 +3,61 @@ provider "aws" {
   region     = "ap-southeast-2"
   }
 
+module "asg" {
 
-resource "aws_vpc" "main_vpc" {
-  cidr_block       = "10.0.0.0/16"
+  source = "./asg"
   
-  tags = {
-    Name = "main"
+}
+
+module "vpc" {
+
+  source = "./vpc"
+  
+}
+
+module "sg" {
+
+  source = "./sg"
+  
+}
+
+
+
+resource "aws_elb" "my_elb" {
+  name = "my-alb"
+  subnets = [ module.vpc.subnet1_output_id,module.vpc.subnet2_output_id]
+  security_groups = [module.sg.sg_id.id]
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port =80
+    lb_protocol = "http"
   }
+
+health_check {
+  healthy_threshold = 2
+  unhealthy_threshold = 2
+  timeout =3
+  target = "HTTP:80/"
+  interval = 30
+}
+
+cross_zone_load_balancing = true
+connection_draining = true  
+connection_draining_timeout = 400
+
+
+
+tags = {
+  Name = "My_ELB"
+}
+
+}
+
+# Stitch Up the ELB with the ASG
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = module.asg.asg_id.id
+  elb                    = aws_elb.my_elb.id
 }
